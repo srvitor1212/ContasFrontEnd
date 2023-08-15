088f23d48529f21f4f01a8f24a43734848252c5d
Vitor - PC <vitor.email2011@gmail.com>
2023-08-14 21:56:47 -0300

    Validação de valor

--- Model/Entrada.cs
+++ Model/Entrada.cs
@@ -7,6 +7,8 @@ namespace ContasFrontEnd.Model
         [Required(ErrorMessage = "Informe um nome como: Salário, Aluguel, Venda do Carro...")]
         [MinLength(3, ErrorMessage = "Precisa ter no mínimo 3 caracteres. Exemplo: Salário, Aluguel, Venda do Carro...")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe o valor recebido mensalmente")]
         public double Valor { get; set; }
         public DateTime DataVigencia { get; set; }
     }
