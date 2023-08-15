56084ba198105849afecbc5db4401faddd853843
Vitor - PC <vitor.email2011@gmail.com>
2023-08-14 21:51:58 -0300

    Mensagem no form

--- Model/Entrada.cs
+++ Model/Entrada.cs
@@ -5,7 +5,7 @@ namespace ContasFrontEnd.Model
     public class Entrada
     {
         [Required(ErrorMessage = "Informe um nome como: Salário, Aluguel, Venda do Carro...")]
-        [MinLength(3, ErrorMessage = "Precisa ter no mínimo 3 caracteres.")]
+        [MinLength(3, ErrorMessage = "Precisa ter no mínimo 3 caracteres. Exemplo: Salário, Aluguel, Venda do Carro...")]
         public string Nome { get; set; }
         public double Valor { get; set; }
         public DateTime DataVigencia { get; set; }
