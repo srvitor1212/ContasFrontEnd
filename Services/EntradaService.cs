bfab87d47c4a68beac5d0e5cfd9a60edad09adcb
Vitor - PC <vitor.email2011@gmail.com>
2023-08-18 11:46:21 -0300

    Criando service de Entrada

---
+++ Services/EntradaService.cs
@@ -0,0 +1,12 @@
+﻿using ContasFrontEnd.Model;
+
+namespace ContasFrontEnd.Services
+{
+    public class EntradaService : IEntradaService
+    {
+        Task<List<Entrada>> IEntradaService.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
