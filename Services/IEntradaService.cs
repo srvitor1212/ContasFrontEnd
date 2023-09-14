4bccf76267d72d205c1bc77bc047e91c6fe7a5a4
Vitor - PC <vitor.email2011@gmail.com>
2023-09-14 18:36:25 -0300

    Form com campo data corrigido, opção loading, retornar HttpResponseMessage na service

--- Services/IEntradaService.cs
+++ Services/IEntradaService.cs
@@ -5,6 +5,6 @@ namespace ContasFrontEnd.Services
     public interface IEntradaService
     {
         Task<List<Entrada>> GetAll();
-        Task<Entrada> Create(Entrada entrada);
+        Task<HttpResponseMessage> Create(Entrada entrada);
     }
 }
