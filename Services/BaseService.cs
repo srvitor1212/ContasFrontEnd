3a844310f64f02762a6e125fc0f4a32871c5f248
Vitor - PC <vitor.email2011@gmail.com>
2023-10-05 15:27:28 -0300

    URL de produção

--- Services/BaseService.cs
+++ Services/BaseService.cs
@@ -2,8 +2,8 @@
 {
     public class BaseService
     {
-        //public string BaseURL { get; private set; } = "https://contaswebapi.azurewebsites.net/";
-        public string BaseURL { get; private set; } = "https://localhost:5001/";
+        public string BaseURL { get; private set; } = "https://contaswebapi.azurewebsites.net/";
+        //public string BaseURL { get; private set; } = "https://localhost:5001/";
 
         // https://localhost:5001/
         // https://contaswebapi.azurewebsites.net/
