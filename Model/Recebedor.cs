9b02074648e6a4480e1d6c5f702cabf918e776ab
Vitor - PC <vitor.email2011@gmail.com>
2023-10-18 17:02:01 -0300

    Model recebedor, botão adicionar, Página index do recebedores

---
+++ Model/Recebedor.cs
@@ -0,0 +1,10 @@
+﻿namespace ContasFrontEnd.Model
+{
+    public class Recebedor
+    {
+        public int Id { get; set; }
+        public string Nome { get; private set; }
+
+        //todo:public ICollection<Dividas> Dividas { get; private set; }
+    }
+}
