1ac9daa52a46720053574bfd25601f2bcf053e6a
Vitor - PC <vitor.email2011@gmail.com>
2023-12-15 14:39:21 -0300

    Criação do esqueleto da service Recebedor

---
+++ Services/IRecebedorService.cs
@@ -0,0 +1,12 @@
+﻿using ContasFrontEnd.Model;
+
+namespace ContasFrontEnd.Services
+{
+    public interface IRecebedorService
+    {
+        Task<List<Recebedor>> GetAll();
+        Task<HttpResponseMessage> Create(Recebedor recebedor);
+        Task<Recebedor> GetById(int id);
+        Task<HttpResponseMessage> Delete(int id);
+    }
+}
---
+++ Services/RecebedorService.cs
@@ -0,0 +1,27 @@
+﻿using ContasFrontEnd.Model;
+
+namespace ContasFrontEnd.Services
+{
+    public class RecebedorService : BaseService, IEntradaService
+    {
+        public Task<HttpResponseMessage> Create(Entrada entrada)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Entrada>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Entrada> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
